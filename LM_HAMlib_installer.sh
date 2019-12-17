ee36a9f6a851bb61ff275f15dca15c5f282486ff
Alex van de Pas <alexvandepas@MBPro-Alex.vandepas.lan>
2019-12-17 21:38:10 +0100

    code cleanup

--- LM_HAMlib_installer.sh
+++ LM_HAMlib_installer.sh
@@ -5,7 +5,7 @@ clear
 if [ "$EUID" -ne 0 ]
   then 
   echo "Please run script with sudo!"
-  echo "like this: user@hostname:~$ sudo ./LM_CQRlog_installer_incl_HAMlib.sh "
+  echo "like this: user@hostname:~$ sudo ./LM_HAMlib_installer.sh "
   echo "your password will be asked...."
   exit
 else
@@ -25,41 +25,6 @@ VER=3.3
 FILE=hamlib-$VER.tar.gz
 FOLDER=hamlib-$VER
 LINK=https://github.com/Hamlib/Hamlib/releases/download/$VER/$FILE
-
-
-
-
-#adding OK2CQR PPA to repositories
-echo "adding OK2CQR PPA to repositories"
-sleep 5
-add-apt-repository -y ppa:ok2cqr/ppa
-
-#Running apt-get update
-clear
-echo "Running apt-get update"
-sleep 5
-apt-get update
-
-#Installing CQRlog
-clear
-echo "Installing CQRlog"
-sleep 5
-apt-get -y install cqrlog
-
-#checking if user is a dialout member
-clear
-echo "checking if user: $SUDO_USER , is a member of the dialout group" 
-
-dialout=$(groups $SUDO_USER|grep dialout)
-if [[ "$dialout" = "dialout" ]]; then
-    echo "User is a member of the dialout group"
-else
-    echo "user is not a member of the dialout group"
-    echo "Adding user to the dialout group"
-    usermod -a -G dialout $SUDO_USER
-fi
-
-
 
 #installing build tools
 clear
@@ -125,17 +90,10 @@ rm -R $FOLDER
 rmdir $FOLDER
 rm $FILE
 
-
 #Done installing.
+sleep 5
 clear
-echo "****************************************************"
-echo "****************************************************"
-echo "**             Installation is finished           **"
-echo "**    FORCE REBOOTING YOUR SYSTEM IN 30 SECONDS ! **"
-echo "****************************************************"
-echo "****************************************************"
-
-
-sleep 30
-reboot
-
+echo "***********************************"
+echo "*     Installation is finished    *"
+echo "*  PLEASE REBOOT YOUR SYSTEM NOW  *"
+echo "***********************************"
